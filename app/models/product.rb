2314798998f38d92ab6e7945301b333da74aca5f
Valentin Chauveau <valentin.korea@gmail.com>
2022-07-26 18:49:23 +0200

    remove total price from order

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   has_many :line_items, dependent: :destroy
+  monetize :price_cents
+  register_currency :eur
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_163252) do
+ActiveRecord::Schema.define(version: 2022_07_26_164850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2022_07_26_163252) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.float "total_price"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
