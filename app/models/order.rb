66be39247078d7b3fe4bbee327db9c9bceab44b1
Valentin Chauveau <valentin.korea@gmail.com>
2022-08-04 19:15:04 +0200

    add elements to order controller & views

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user, optional: true
   has_many :line_items, dependent: :destroy
+
+  def sub_total
+    sum = 0
+    self.line_items.each { |line_item| sum += line_item.total_price }
+    return ::ApplicationController.helpers.number_to_euro(sum)
+  end
 end
